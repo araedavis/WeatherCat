5ae3ebd5acaefed1beed3c70eb9723cb26b77aa9
Allison Davis <a.rae.davis@gmail.com>
2016-03-07 19:34:49 -0800

    bug fix

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ function loadWeather() {
     url:  forecastURL,
     jsonpCallback: 'jsonpCallback',
     contentType: 'application/json',
-    dataType: 'jsonp',
+    dataType: 'json',
     exclude: 'hourly',
     success: function(json) {
       $('#currentTemp').html(json.currently.temperature + "&#176");
